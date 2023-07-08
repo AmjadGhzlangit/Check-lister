dbe2ae027f553284a9b267cd610af38adc9342b2
Amjad Ghzlan <amjadghzlangit@github.com>
2023-07-08 12:57:47 +0300

    add Admin role and write Feature test for admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,46 +42,7 @@
         <link href="vendors/@coreui/chartjs/css/coreui-chartjs.css" rel="stylesheet">
       </head>
       <body>
-        <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
-          <div class="sidebar-brand d-none d-md-flex">
-            <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-              <use xlink:href="{{ asset('assets/brand/coreui.svg#full')}}"></use>
-            </svg>
-            <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-              <use xlink:href="{{ asset('assets/brand/coreui.svg#signet')}}"></use>
-            </svg>
-          </div>
-          <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
-            <li class="nav-item"><a class="nav-link" href="index.html">
-                <svg class="nav-icon">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
-                </svg> Dashboard<span class="badge badge-sm bg-info ms-auto">NEW</span></a></li>
-           
-            <li class="nav-title">Components</li>
-            <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
-                <svg class="nav-icon">
-                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
-                </svg> Base</a>
-              <ul class="nav-group-items">
-                <li class="nav-item"><a class="nav-link" href="base/accordion.html"><span class="nav-icon"></span> Accordion</a></li>
-                <li class="nav-item"><a class="nav-link" href="base/breadcrumb.html"><span class="nav-icon"></span> Breadcrumb</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-               <a  class="nav-link" href="{{ route('logout') }}"
-                 onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-                           <svg class="nav-icon">
-                            
-                              <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
-                          </svg>
-                          {{ __('Log Out') }}
-                  </a>
-                <form id="logout-form" method="POST" action="{{ route('logout') }}">
-                    @csrf   
-                </form>
-         </li>
-        </div>
+        @include('layouts.sidebar')
         <div class="wrapper d-flex flex-column min-vh-100 bg-light">
           <header class="header header-sticky mb-4">
             <div class="container-fluid">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PagesController as AdminPagesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware(['auth'])->group(function(){
 
+    Route::prefix('admin')->as('admin.')->middleware('is_admin')->group(function () {
+        Route::resource('pages',AdminPagesController::class);
+    });
+});
 require __DIR__.'/auth.php';
